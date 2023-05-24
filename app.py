ea276fc8d0c19db8fc2186be956a9ee21f0d71af
DannoNinno <daniel.progra.work@gmail.com>
2023-05-24 15:59:21 -0400

    feat: Modelo Sentimiento completado para resultados mostrables

--- app.py
+++ app.py
@@ -5,22 +5,15 @@ from models import pySentimentModel
 app = Flask(__name__);
 @app.route('/')
 def main():
-    return "<h1>Se ha conectado Correctamente</h1>"
-
-@app.route('/Detoxify')
-def detoxifyPredictor():
-
-    print("Probando Servicio....")
+    return "<h1>Se ha conectado Correctamente</h1>" 
+@app.route('/Detoxify/Analysis/<stringInput>')
+def detoxifyPredictor(stringInput):
+    print("Probando Servicio...." + stringInput)
     print("IA inicializada, Probando Frase....")
     return detoxifyModel.initDetoxify()
-
-@app.route('/PySentimiento')
-def emotionPredictor():
-
-    print("Probando Servicio....")
-    print("IA inicializada, Probando Frase....")
-    return pySentimentModel.initSentimiento()
-
+@app.route('/PySentimiento/Analysis/<modelType>/<stringInput>')
+def emotionPredictor(stringInput,modelType):
+    print("Probando Servicio.... con la frase: "+ stringInput)
+    return "<h3>"+pySentimentModel.sentimientoPredictor(modelType,stringInput)+"</h3>"
 if __name__ == '__main__':
-
     app.run(debug=True, port=4000)
--- models/pySentimentModel.py
+++ models/pySentimentModel.py
@@ -1,4 +1,12 @@
 from pysentimiento import create_analyzer
 
-def initSentimiento():
-    return "sentimiento"
+def sentimientoPredictor(modelType, inputString):
+    if(modelType=="STM"):
+        predictor = create_analyzer(task="sentiment", lang="es")
+    if(modelType=="EMT"):
+        predictor = create_analyzer(task="emotion", lang="es")
+    if(modelType=="HFS"):
+        predictor = create_analyzer(task="hate_speech", lang="es")
+    response = predictor.predict(inputString)
+    return str(response)
+
